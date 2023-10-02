3c1e90c8652f0a5063cf8a0825625e17a2b37cd8
Jacob <jakxw2@mst.com>
2023-10-02 13:22:21 -0500

    created normalization function shells

---
+++ normalizer.py
@@ -0,0 +1,57 @@
+import table
+
+def first_normal_form(table: table.Table) -> list[table.Table]:
+    '''
+    Takes in a table and returns a list of tables
+    These tables store an equivalent amount of data as the inputed table
+    The tables returned will be in first normal forms
+    '''
+    pass
+
+def second_normal_form(table: table.Table) -> list[table.Table]:
+    '''
+    Takes in a table and returns a list of tables
+    These tables store an equivalent amount of data as the inputed table
+    The tables returned will be in second normal forms
+    '''
+    pass
+
+def third_normal_form(table: table.Table) -> list[table.Table]:
+    '''
+    Takes in a table and returns a list of tables
+    These tables store an equivalent amount of data as the inputed table
+    The tables returned will be in third normal forms
+    '''
+    pass
+
+def third_normal_form(table: table.Table) -> list[table.Table]:
+    '''
+    Takes in a table and returns a list of tables
+    These tables store an equivalent amount of data as the inputed table
+    The tables returned will be in third normal forms
+    '''
+    pass
+
+def boyce_codd_normal_form(table: table.Table) -> list[table.Table]:
+    '''
+    Takes in a table and returns a list of tables
+    These tables store an equivalent amount of data as the inputed table
+    The tables returned will be in boyce codd normal forms
+    '''
+    pass
+
+def forth_normal_form(table: table.Table) -> list[table.Table]:
+    '''
+    Takes in a table and returns a list of tables
+    These tables store an equivalent amount of data as the inputed table
+    The tables returned will be in fourth normal forms
+    '''
+    pass
+
+def fifth_normal_form(table: table.Table) -> list[table.Table]:
+    '''
+    Takes in a table and returns a list of tables
+    These tables store an equivalent amount of data as the inputed table
+    The tables returned will be in fifth normal forms
+    '''
+    pass
