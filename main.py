b209db39b85897162f106ea025273b81f30675a5
Jacob <jakxw2@mst.com>
2023-11-01 03:52:10 -0500

    added normalization logic to main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import table
 import csv_parser
+import normalizer
 
 def input_funct_depends(my_table: table.Table) -> None:
     '''
@@ -66,6 +67,46 @@ def create_table() -> table.Table:
             print(err)
         except RuntimeError as err:
             print(err)
+            
+def normalize_to_form(start_table: table.Table, form: int) -> list[table.Table]:
+    '''
+    This will take in a table object and a form as an int 1 -> 1st, 4 -> bc, 6 -> 5th\n
+    This outputs a list of tables normalized to the given form
+    '''
+    form_counter = 0
+    table_list = [start_table]
+    while form_counter != form:
+        print()
+        form_counter += 1
+        new_table_list: list[table.Table] = []
+        for my_table in table_list:
+            match form_counter:
+                case 1:
+                    print("Normalized to 1st normal form:")
+                    new_table_list += normalizer.first_normal_form(my_table)
+                case 2:
+                    print("Normalized to 2nd normal form:")
+                    new_table_list += normalizer.second_normal_form(my_table)
+                case 3:
+                    print("Normalized to 3rd normal form:")
+                    new_table_list += normalizer.third_normal_form(my_table)
+                case 4:
+                    print("Normalized to Boyce Codd normal form:")
+                    new_table_list += normalizer.boyce_codd_normal_form(my_table)
+                case 5:
+                    print("Normalized to 4th normal form:")
+                    new_table_list += normalizer.forth_normal_form(my_table)
+                case 6:
+                    print("Normalized to 5th normal form:")
+                    new_table_list += normalizer.fifth_normal_form(my_table)
+                case _:
+                    raise RuntimeError(f"What.")
+        for new_table in new_table_list:
+            new_table.print_table()
+            new_table.print_primary_key()
+            new_table.print_functional_dependencies()
+        table_list = new_table_list
+    return table_list
 
 def main():
     my_table = create_table()
@@ -73,9 +114,23 @@ def main():
     input_primary_key(my_table)
     input_funct_depends(my_table)
     
+    normal_form = int(input(
+        "Please enter the form you would like to normalize to\n"
+        "1) First normal form\n"
+        "2) Second normal form\n"
+        "3) Third normal form\n"
+        "4) Boyce Codd normal form\n"
+        "5) Fourth normal form\n"
+        "6) Fifth normal form\n"
+        "Form: "
+    ))
+    
+    print("\nOriginal Table:")
     my_table.print_table()
     my_table.print_primary_key()
     my_table.print_functional_dependencies()
+    
+    my_normalized_tables = normalize_to_form(my_table, normal_form)
     
 if __name__ == "__main__":
     main()
